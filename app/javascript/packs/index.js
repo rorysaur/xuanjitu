b1f0f749fb4b170aa69cef0288cd4a1cf524cd84
rorysaur <ou.rory@gmail.com>
2019-10-19 01:22:02 +0200

    render characters from backend
    + also extract constants

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,187 +1,175 @@
-const characters = [
-  {
-    id: 1,
-    text: '一',
-    x: 0,
-    y: 0,
-    color: 'black'
+const constants = {
+  background: {
+    width: 750,
+    height: 750,
+    color: '#eee',
   },
-  {
-    id: 2,
-    text: '二',
-    x: 1,
-    y: 0,
-    color: 'black'
-  },
-  {
-    id: 3,
-    text: '三',
-    x: 2,
-    y: 0,
-    color: 'black'
-  },
-  {
-    id: 4,
-    text: '四',
-    x: 0,
-    y: 1,
-    color: 'black'
-  },
-  {
-    id: 5,
-    text: '五',
-    x: 1,
-    y: 1,
-    color: 'green'
-  },
-  {
-    id: 6,
-    text: '六',
-    x: 2,
-    y: 1,
-    color: 'green'
-  },
-  {
-    id: 7,
-    text: '七',
-    x: 0,
-    y: 2,
-    color: 'black'
-  },
-  {
-    id: 8,
-    text: '八',
-    x: 1,
-    y: 2,
-    color: 'green'
-  },
-  {
-    id: 9,
-    text: '九',
-    x: 2,
-    y: 2,
-    color: 'green'
-  },
-];
-
-let stage = new Konva.Stage({
-  container: 'container',   // id of container <div>
-  width: window.innerWidth,
-  height: window.innerHeight
-});
-
-let layer = new Konva.Layer();
-
-let characterTexts = characters.map((character) => {
-  const newText = new Konva.Text({
-    x: character.x * 25,
-    y: character.y * 25,
-    text: character.text,
-    fontFamily: 'Ma Shan Zheng',
+  characters: {
+    width: 25,
+    height: 25,
+    colorMappings: {
+      black: 'black',
+      blue: 'blue',
+      green: 'green',
+      purple: 'purple',
+      red: 'red',
+      yellow: '#fc0',
+    },
     fontSize: 20,
-    fill: character.color
+  },
+  fadeIn: {
+    maxDuration: 6,
+  },
+  focusText: {
+    marginLeft: 20,
+    fontSize: 200,
+  },
+  stage: {
+    height: 750,
+  },
+  text: {
+    offset: {
+      x: 10,
+      y: 10,
+    }
+  },
+  trace: {
+    color: "#999",
+    strokeWidth: 2,
+    fadeDuration: 1,
+  }
+};
+
+const render = ({ characters }) => {
+  let stage = new Konva.Stage({
+    container: 'container',   // id of container <div>
+    width: window.innerWidth,
+    height: constants.stage.height,
   });
 
-  return newText;
-});
+  let layer = new Konva.Layer();
 
-let characterWidth = characterTexts[0].width();
-let characterHeight = characterTexts[0].height();
+  let characterTexts = characters.map((character) => {
+    const { width, height, colorMappings, fontSize } = constants.characters;
+    const { offset } = constants.text;
 
-let gridBackground = new Konva.Rect({
-  x: 0,
-  y: 0,
-  strokeWidth: 5,
-  fill: '#eee',
-  width: 750,
-  height: 750,
-});
-layer.add(gridBackground);
-
-let focusText = new Konva.Text({
-  x: gridBackground.width() + 20,
-  y: 0,
-  text: '',
-  fontFamily: 'Ma Shan Zheng',
-  fontSize: 96,
-  fill: 'black',
-});
-layer.add(focusText);
-
-const getCenterX = (character) => {
-  return character.x() + (characterWidth / 2);
-}
-const getCenterY = (character) => {
-  return character.y() + (characterHeight / 2);
-}
-
-let lastFocusedCharacter, lastLine;
-
-const updateFocusText = (characterText) => {
-  focusText.text(characterText.text());
-  focusText.fill(characterText.fill());
-}
-
-const updateTrace = (characterText) => {
-  if (!lastFocusedCharacter) { return }
-
-  // make new line
-  let newLine = new Konva.Line({
-    points: [
-      getCenterX(lastFocusedCharacter),
-      getCenterY(lastFocusedCharacter),
-      getCenterX(characterText),
-      getCenterY(characterText)
-    ],
-    fill: '#999',
-    stroke: '#999',
-    strokeWidth: 1
-  });
-  layer.add(newLine);
-
-  if (lastLine) {
-    // fade last line
-    let oldLine = lastLine;
-    let tween = new Konva.Tween({
-      node: oldLine,
-      duration: 1,
-      points: [
-        oldLine.points()[2],
-        oldLine.points()[3],
-        oldLine.points()[2],
-        oldLine.points()[3]
-      ],
-      onFinish: () => { oldLine.destroy() }
+    const newText = new Konva.Text({
+      x: (character.x_coordinate * width) + offset.x,
+      y: (character.y_coordinate * height) + offset.y,
+      text: character.text,
+      fontFamily: 'Ma Shan Zheng',
+      fontSize: fontSize,
+      fill: constants.characters.colorMappings[character.color],
     });
 
-    tween.play();
+    return newText;
+  });
+
+  let characterWidth = characterTexts[0].width();
+  let characterHeight = characterTexts[0].height();
+
+  let gridBackground = new Konva.Rect({
+    x: 0,
+    y: 0,
+    strokeWidth: 5,
+    fill: constants.background.color,
+    width: constants.background.width,
+    height: constants.background.height,
+  });
+  layer.add(gridBackground);
+
+  let focusText = new Konva.Text({
+    x: gridBackground.width() + constants.focusText.marginLeft,
+    y: 0,
+    text: '',
+    fontFamily: 'Ma Shan Zheng',
+    fontSize: constants.focusText.fontSize,
+  });
+  layer.add(focusText);
+
+  const getCenterX = (character) => {
+    return character.x() + (characterWidth / 2);
+  }
+  const getCenterY = (character) => {
+    return character.y() + (characterHeight / 2);
   }
 
-  lastLine = newLine;
-}
+  let lastFocusedCharacter, lastLine;
 
-const characterTextMouseover = (characterText) => {
-  updateFocusText(characterText);
-  updateTrace(characterText);
-  layer.draw();
+  const updateFocusText = (characterText) => {
+    focusText.text(characterText.text());
+    focusText.fill(characterText.fill());
+  }
 
-  lastFocusedCharacter = characterText;
-}
+  const updateTrace = (characterText) => {
+    if (!lastFocusedCharacter) { return }
 
-characterTexts.forEach((characterText) => {
-  layer.add(characterText);
+    const { color, strokeWidth, fadeDuration } = constants.trace;
 
-  // fade in text
-  characterText.opacity(0);
-  let fadeIn = new Konva.Tween({
-    node: characterText,
-    duration: 0.5 + (Math.random() * 5),
-    opacity: 1
+    // make new line
+    let newLine = new Konva.Line({
+      points: [
+        getCenterX(lastFocusedCharacter),
+        getCenterY(lastFocusedCharacter),
+        getCenterX(characterText),
+        getCenterY(characterText)
+      ],
+      stroke: color,
+      strokeWidth: strokeWidth,
+    });
+    layer.add(newLine);
+
+    if (lastLine) {
+      // fade last line
+      let oldLine = lastLine;
+      let tween = new Konva.Tween({
+        node: oldLine,
+        duration: fadeDuration,
+        points: [
+          oldLine.points()[2],
+          oldLine.points()[3],
+          oldLine.points()[2],
+          oldLine.points()[3]
+        ],
+        onFinish: () => { oldLine.destroy() }
+      });
+
+      tween.play();
+    }
+
+    lastLine = newLine;
+  }
+
+  const characterTextMouseover = (characterText) => {
+    updateFocusText(characterText);
+    updateTrace(characterText);
+    layer.draw();
+
+    lastFocusedCharacter = characterText;
+  }
+
+  characterTexts.forEach((characterText) => {
+    layer.add(characterText);
+
+    // fade in text
+    characterText.opacity(0);
+    let fadeIn = new Konva.Tween({
+      node: characterText,
+      duration: Math.random() * constants.fadeIn.maxDuration,
+      opacity: 1
+    });
+    fadeIn.play();
+
+    characterText.on('mouseover', characterTextMouseover.bind(this, characterText));
   });
-  fadeIn.play();
 
-  characterText.on('mouseover', characterTextMouseover.bind(this, characterText));
+  stage.add(layer);
+  layer.draw();
+}
+
+$.ajax({
+  url: '/characters.json'
+})
+.done((data) => {
+  render(data);
 });
-
-stage.add(layer);
-layer.draw();
