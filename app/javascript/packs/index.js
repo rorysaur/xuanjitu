e1420572b0dc44d80292a49ad41c9ccee0aef6dd
rorysaur <ou.rory@gmail.com>
2019-10-18 17:55:32 +0200

    render some hard-coded characters with focus sidebar

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,30 +1,120 @@
-// Konva sample code
+const characters = [
+  {
+    id: 1,
+    text: '一',
+    x: 0,
+    y: 0,
+    color: 'black'
+  },
+  {
+    id: 2,
+    text: '二',
+    x: 1,
+    y: 0,
+    color: 'black'
+  },
+  {
+    id: 3,
+    text: '三',
+    x: 2,
+    y: 0,
+    color: 'black'
+  },
+  {
+    id: 4,
+    text: '四',
+    x: 0,
+    y: 1,
+    color: 'black'
+  },
+  {
+    id: 5,
+    text: '五',
+    x: 1,
+    y: 1,
+    color: 'green'
+  },
+  {
+    id: 6,
+    text: '六',
+    x: 2,
+    y: 1,
+    color: 'green'
+  },
+  {
+    id: 7,
+    text: '七',
+    x: 0,
+    y: 2,
+    color: 'black'
+  },
+  {
+    id: 8,
+    text: '八',
+    x: 1,
+    y: 2,
+    color: 'green'
+  },
+  {
+    id: 9,
+    text: '九',
+    x: 2,
+    y: 2,
+    color: 'green'
+  },
+];
 
-// first we need to create a stage
-var stage = new Konva.Stage({
+let stage = new Konva.Stage({
   container: 'container',   // id of container <div>
-  width: 500,
-  height: 500
+  width: window.innerWidth,
+  height: window.innerHeight
 });
 
-// then create layer
-var layer = new Konva.Layer();
+let layer = new Konva.Layer();
 
-// create our shape
-var circle = new Konva.Circle({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  radius: 70,
-  fill: 'red',
-  stroke: 'black',
-  strokeWidth: 4
+let characterTexts = characters.map((character) => {
+  const newText = new Konva.Text({
+    x: character.x * 20,
+    y: character.y * 20,
+    text: character.text,
+    fontSize: 16,
+    fill: character.color
+  });
+
+  return newText;
 });
 
-// add the shape to the layer
-layer.add(circle);
+let gridBackground = new Konva.Rect({
+  x: 0,
+  y: 0,
+  strokeWidth: 5,
+  fill: '#eee',
+  width: 600,
+  height: 600,
+});
 
-// add the layer to the stage
+let focusText = new Konva.Text({
+  x: gridBackground.width() + 20,
+  y: 0,
+  text: '',
+  fontSize: 72,
+  fill: 'black',
+});
+
+layer.add(gridBackground);
+layer.add(focusText);
+
+characterTexts.forEach((characterText) => {
+  layer.add(characterText);
+
+  characterText.on('mouseover', () => {
+    // change the focusText and redraw
+    focusText.text(characterText.text());
+    focusText.fill(characterText.fill());
+    layer.draw();
+  });
+});
+
 stage.add(layer);
 
-// draw the image
 layer.draw();
