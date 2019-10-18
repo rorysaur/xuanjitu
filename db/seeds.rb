cd4a58fee89616b0f6631f86c8faec4a123fcc1e
rorysaur <ou.rory@gmail.com>
2019-10-18 21:56:50 +0200

    add seeds code for positions and characters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# seed positions if table is empty
+if !Position.any?
+  ActiveRecord::Base.transaction do
+    (0..28).each do |x|
+      (0..28).each do |y|
+        Position.create!(x_coordinate: x, y_coordinate: y)
+      end
+    end
+  end
+else
+  puts "#{Position.count} positions already exist!"
+end
+
+# seed characters if table is empty
+if !Character.any?
+  file_path = File.join(Rails.root, "db", "data", "characters.txt")
+  text = File.read(file_path)
+
+  ActiveRecord::Base.transaction do
+    lines = text.split("\n")
+    lines.each_with_index do |line, line_idx|
+      characters = line.split("")
+      characters.each_with_index do |char, char_idx|
+        position = Position.find_by!(
+          x_coordinate: char_idx,
+          y_coordinate: line_idx,
+        )
+
+        Character.create!(
+          text: char,
+          form: Character.forms[:simplified],
+          position: position,
+        )
+      end
+    end
+  end
+else
+  puts "#{Character.count} characters already exist!"
+end
