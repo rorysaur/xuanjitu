bf34f9250f0646166426178931dcb174786f9a89
rorysaur <ou.rory@gmail.com>
2019-10-18 17:01:18 +0200

    add konva library and draw a red circle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("konva")
+require("packs/index")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
