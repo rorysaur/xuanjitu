bbe1ec456547e75a63ea0b5031e7fb5958e15e81
rorysaur <ou.rory@gmail.com>
2020-07-12 19:08:39 -0700

    move constants out to a separate module

---
+++ app/javascript/packs/constants.ts
@@ -0,0 +1,76 @@
+const constants = {
+  background: {
+    width: 750,
+    height: 750,
+    color: '#eee',
+  },
+  buttons: {
+    marginLeft: 20,
+    y: 30,
+  },
+  characters: {
+    width: 25,
+    height: 25,
+    colorMappings: {
+      black: 'black',
+      blue: 'blue',
+      green: 'green',
+      purple: 'purple',
+      red: 'red',
+      yellow: '#fc0',
+    },
+    fontSize: 20,
+    strokeWidth: 1,
+  },
+  characterStates: {
+    faded: {
+      fill: '#999',
+      opacity: 0.4,
+      characterState: 'faded',
+    },
+    highlighted: {
+      fill: 'red',
+      opacity: 0.4,
+      characterState: 'highlighted',
+    },
+    selected: {
+      fill: 'red',
+      opacity: 1,
+      characterState: 'selected',
+    }
+  },
+  fadeIn: {
+    maxDuration: 6,
+  },
+  focusText: {
+    marginLeft: 20,
+    fontSize: 200,
+    y: 500
+  },
+  instructionText: {
+    marginLeft: 20,
+    y: 100,
+  },
+  readingText: {
+    lineHeight: 45,
+    marginLeft: 20,
+    fontSize: 30,
+    y: 300,
+  },
+  stage: {
+    height: 750,
+  },
+  text: {
+    offset: {
+      x: 10,
+      y: 10,
+    }
+  },
+  trace: {
+    color: "#999",
+    strokeWidth: 2,
+    fadeDuration: 1,
+  }
+};
+
+export default constants;
--- app/javascript/packs/index.ts
+++ app/javascript/packs/index.ts
@@ -1,79 +1,5 @@
 import Konva from 'konva';
-
-const constants = {
-  background: {
-    width: 750,
-    height: 750,
-    color: '#eee',
-  },
-  buttons: {
-    marginLeft: 20,
-    y: 30,
-  },
-  characters: {
-    width: 25,
-    height: 25,
-    colorMappings: {
-      black: 'black',
-      blue: 'blue',
-      green: 'green',
-      purple: 'purple',
-      red: 'red',
-      yellow: '#fc0',
-    },
-    fontSize: 20,
-    strokeWidth: 1,
-  },
-  characterStates: {
-    faded: {
-      fill: '#999',
-      opacity: 0.4,
-      characterState: 'faded',
-    },
-    highlighted: {
-      fill: 'red',
-      opacity: 0.4,
-      characterState: 'highlighted',
-    },
-    selected: {
-      fill: 'red',
-      opacity: 1,
-      characterState: 'selected',
-    }
-  },
-  fadeIn: {
-    maxDuration: 6,
-  },
-  focusText: {
-    marginLeft: 20,
-    fontSize: 200,
-    y: 500
-  },
-  instructionText: {
-    marginLeft: 20,
-    y: 100,
-  },
-  readingText: {
-    lineHeight: 45,
-    marginLeft: 20,
-    fontSize: 30,
-    y: 300,
-  },
-  stage: {
-    height: 750,
-  },
-  text: {
-    offset: {
-      x: 10,
-      y: 10,
-    }
-  },
-  trace: {
-    color: "#999",
-    strokeWidth: 2,
-    fadeDuration: 1,
-  }
-};
+import constants from './constants';
 
 let characterGrid: Konva.Text[][] = new Array(29);
 for (let y = 0; y < characterGrid.length; y++) { // check for loops
@@ -611,11 +537,15 @@ $.when(
   }),
   $.ajax({
     url: '/segments.json'
+  }),
+  $.ajax({
+    url: '/readings.json'
   })
-).then((charactersResponse, segmentsResponse) => {
+).then((charactersResponse, segmentsResponse, readingsResponse) => {
   data = {
     characters: charactersResponse[0]["characters"],
     segments: segmentsResponse[0]["segments"],
+    readings: segmentsResponse[0]["readings"]
   };
   render(data);
   // $('.footer').show();
