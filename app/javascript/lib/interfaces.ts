b8fc3d8096287979356799b4f5881fff52216b60
rorysaur <ou.rory@gmail.com>
2020-07-17 21:36:40 -0700

    reorganize file and split code into more modules

---
+++ app/javascript/lib/interfaces.ts
@@ -0,0 +1,48 @@
+import Konva from 'konva';
+
+interface CharacterData {
+  readonly text: string;
+  readonly form: string;
+  readonly x_coordinate: number;
+  readonly y_coordinate: number;
+  readonly color: string;
+  readonly rhyme: boolean;
+  readonly segment_ids: number[];
+}
+
+interface ReadingData {
+  readonly id: number;
+  readonly color: string;
+  readonly block_number: number;
+  readonly number: number;
+  readonly length: number;
+  readonly segment_ids: number[];
+}
+
+interface SegmentData {
+  readonly id: number;
+  readonly head_x: number;
+  readonly head_y: number;
+  readonly tail_x: number;
+  readonly tail_y: number;
+  readonly length: number;
+  readonly color: string;
+}
+
+interface State {
+  demo: {
+    currentReading: {
+      index: number;
+      length: number;
+    }
+    currentSidebarGroup: undefined | Konva.Group;
+    highlightedChars: Konva.Text[];
+  }
+}
+
+export {
+  CharacterData,
+  ReadingData,
+  SegmentData,
+  State,
+}
