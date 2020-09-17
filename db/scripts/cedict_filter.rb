93badfb5eedb5088bf4fe2a97f8a50974b9f0ad0
rorysaur <ou.rory@gmail.com>
2020-09-16 19:20:13 -0700

    fix pinyin diacritics

--- db/scripts/cedict_filter.rb
+++ db/scripts/cedict_filter.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class CedictFilter
   def initialize
     @csv_rows = CSV.parse(File.read(source_csv_path), headers: true)
@@ -12,9 +14,14 @@ class CedictFilter
       characters.include?(csv_row["simplified"])
     end
 
+    final_rows = filtered_rows.each do |filtered_row|
+      pinyin = filtered_row["pinyin-diacritic"]
+      filtered_row["pinyin-diacritic"] = replace_diacritics(pinyin)
+    end
+
     CSV.open(destination_csv_path, "wb") do |csv|
       csv << headers
-      filtered_rows.each do |row|
+      final_rows.each do |row|
         csv << row.fields
       end
     end
@@ -36,5 +43,26 @@ class CedictFilter
 
   def source_csv_path
     "./tmp/cc-cedict-gdrive.csv"
+  end
+
+  def replace_diacritics(original_pinyin)
+    return original_pinyin unless original_pinyin.match?("u:")
+
+    final_pinyin = original_pinyin.dup
+
+    diacritics_mapping.each do |a, b|
+      final_pinyin.gsub!(a, b)
+    end
+
+    final_pinyin
+  end
+
+  def diacritics_mapping
+    {
+      "u:1" => "ǖ",
+      "u:2" => "ǘ",
+      "u:3" => "ǚ",
+      "u:4" => "ǜ",
+    }
   end
 end
